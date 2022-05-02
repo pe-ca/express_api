6a779d30a8e7224d0e7d6530d03b6849dda5b59a
Jesús Perales <jesus@campa.com.mx>
2022-05-02 14:59:32 -0500

    agregado endpoint que crea un explorer

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ app.get('/v1/explorers/:id', (req, res) =>{
     res.status(200).json(explorer)
 })
 
+app.get('/v1/explorers', (req, res) =>{
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body //parametros de un cliente
+    res.status(201).json({message: "Created"})
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
